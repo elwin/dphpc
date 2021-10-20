cb624289a04ec43a772f05665bd63266a332e4c2
Elwin Stephan <elwin.stephan@gmail.com>
2021-10-20 14:17:20 +0200

    cleanup

--- code/src/allreduce/impl.cpp
+++ code/src/allreduce/impl.cpp
@@ -1,7 +1,5 @@
 #include "allreduce/impl.hpp"
 
-#include <iostream>
-
 namespace impls::allreduce {
 
 matrix* allreduce::compute() {
@@ -11,22 +9,6 @@ matrix* allreduce::compute() {
   MPI_Allreduce(current->get_ptr(), result->get_ptr(), result->dimension(), MPI_DOUBLE, MPI_SUM, this->comm);
 
   return result;
-}
-
-std::unique_ptr<double[]> run(
-    MPI_Comm comm, std::unique_ptr<double[]> A, int N, std::unique_ptr<double[]> B, int M, int, int) {
-  auto G = std::make_unique<double[]>(N * M);
-
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++) {
-      G[i * M + j] = A[i] * B[j];
-    }
-  }
-
-  auto result = std::make_unique<double[]>(N * M);
-  MPI_Allreduce(G.get(), result.get(), N * M, MPI_DOUBLE, MPI_SUM, comm);
-
-  return G;
 }
 
 } // namespace impls::allreduce
