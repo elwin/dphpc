1010c22c6c56efbf6f7a78cbd7c65be4178aeefe
patrick96 <p.ziegler96@gmail.com>
2021-10-24 16:19:27 +0200

    Add ci pipeline

---
+++ .github/workflows/summary.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set -x
+"${CXX}" --version
+cmake --version
+set +x
+
+echo "PATH=${PATH}"
+echo "CXX=${CXX}"
+echo "CXXFLAGS=${CXXFLAGS}"
+echo "MAKEFLAGS=${MAKEFLAGS}"
+echo "CMAKE_BUILD_TYPE=${BUILD_TYPE}"
