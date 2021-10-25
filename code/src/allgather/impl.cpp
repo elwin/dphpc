47061fc27b87228456661c8207a04712c59e8174
patrick96 <p.ziegler96@gmail.com>
2021-10-25 18:57:41 +0200

    Optimize allgather

--- code/src/allgather/impl.cpp
+++ code/src/allgather/impl.cpp
@@ -5,41 +5,27 @@ namespace impls::allgather {
 matrix allgather::compute() {
   // Create new combined vector to send
   auto send_vector_length = a.size() + b.size();
-  auto send_vector = vector(send_vector_length);
-  for (unsigned long i = 0; i < a.size(); ++i) {
-    send_vector[i] = a[i];
-  }
-  for (unsigned long i = 0; i < b.size(); ++i) {
-    send_vector[i + a.size()] = b[i];
-  }
+  vector send_vector;
+  send_vector.reserve(send_vector_length);
+  send_vector.insert(send_vector.end(), a.begin(), a.end());
+  send_vector.insert(send_vector.end(), b.begin(), b.end());
 
   // Create receive vector
-  auto rec_vector_length = (a.size() + b.size()) * num_procs;
-  auto rec_vector = vector(rec_vector_length);
+  auto rec_vector = vector(send_vector_length * num_procs);
 
   // Distribute all vectors to all nodes
-  MPI_Allgather(&send_vector[0], send_vector_length, MPI_DOUBLE, &rec_vector[0], send_vector_length, MPI_DOUBLE, comm);
+  MPI_Allgather(
+      send_vector.data(), send_vector_length, MPI_DOUBLE, rec_vector.data(), send_vector_length, MPI_DOUBLE, comm);
 
   // Calculate sum of products on all nodes
   auto result = matrix(a.size(), b.size());
   for (int i = 0; i < num_procs; i++) {
-    // Split vectors again
-    auto A = vector(a.size());
-    auto B = vector(b.size());
-
-    for (unsigned int j = 0; j < send_vector_length; j++) {
-      if (j < a.size()) {
-        A[j] = rec_vector[i * send_vector_length + j];
-      } else {
-        B[j - a.size()] = rec_vector[i * send_vector_length + j];
+    for (size_t x = 0; x < a.size(); x++) {
+      for (size_t y = 0; y < b.size(); y++) {
+        result.get(x, y) += rec_vector[i * send_vector_length + x] * rec_vector[i * send_vector_length + a.size() + y];
       }
     }
-
-    auto current = matrix::outer(A, B);
-
-    result.add(current);
   }
-
   return result;
 }
 
