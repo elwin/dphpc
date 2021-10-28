2f946310ab003889681840ee4140be7f24edf3c1
Elwin Stephan <elwin.stephan@gmail.com>
2021-10-28 16:10:52 +0200

    add some scripts

--- scripts/euler/collect.sh
+++ scripts/euler/collect.sh
@@ -1,13 +1,18 @@
 . config.sh
 
-while read -r job_id; do
+job_path="${bb_output_dir}/jobs"
 
+while read -r job_row; do
+  repetition=$(echo "${job_row}" | awk -F '/' '{print $1}')
+  job_id=$(echo "${job_row}" | awk -F '/' '{print $2}')
+
+  folder="${bb_output_dir}/${repetition}/"
   filename="${job_id}"
 
-  echo $filename
+  mkdir -p "${folder}"
 
-  tail -n +36 "${bb_output_dir}/${filename}" > "${output_dir}/${filename}"
+  tail -n +36 "${folder}/${filename}" >>"${output_dir}/${repetition}.json"
 
-done <$job_list
+done <"${job_path}"
 
 echo "Collected all files in ${output_dir}"
--- scripts/euler/init.sh
+++ scripts/euler/init.sh
@@ -1,4 +1,5 @@
+# call this file using `source init.sh`
+
 source /cluster/apps/local/env2lmod.sh
 
-# Doesn't work really, sorry
-module load gcc/8.2.0 cmake/3.20.3 openmpi/4.0.2
+module load gcc/9.3.0 cmake/3.20.3 openmpi/4.0.2
