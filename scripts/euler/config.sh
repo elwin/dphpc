0af74d47276687b98dd829bf4569385449199228
roy-sc <roy.schubiger@gmail.com>
2021-10-29 11:53:24 +0200

    trying to resolve too much space used

--- scripts/euler/config.sh
+++ scripts/euler/config.sh
@@ -7,8 +7,9 @@ echo "config"
 bb_output_dir="../bb_jobs"
 output_dir="../parsed"
 job_file="${bb_output_dir}/jobs"
+trash_dir="../trash"
 
-mkdir -p "${bb_output_dir}" "${output_dir}"
+mkdir -p "${bb_output_dir}" "${output_dir}" "${trash_dir}"
 
 code_dir='./code'
 build_dir="./code/build_output"
