b3844f2be9448136b16ea1b3b7bb2c53b83fb66f
Elwin Stephan <elwin.stephan@gmail.com>
2021-11-01 11:14:47 +0100

    move wrapper to euler_wrapper

--- scripts/collect_jobs.sh
+++ scripts/collect_jobs.sh
@@ -7,5 +7,5 @@ set -e # Exit on first failure
 # Script checks whether the experiments is finished.
 # If not finished:  exit
 # If finished:      collect files and copy to local machine
-ssh -t euler < ./scripts/wrapper/run_verify_completed.sh
+ssh -t euler < ./scripts/euler_wrapper/run_verify_completed.sh
 scp -r euler:~/dphpc/results/tmp results
