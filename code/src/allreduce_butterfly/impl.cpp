a79c19e47557872260aa475ad640e0af2bca263c
roy-sc <roy.schubiger@gmail.com>
2021-11-08 10:34:13 +0100

    There is a bug for non-powers-of-2 ranks, not found yet

--- code/src/allreduce_butterfly/impl.cpp
+++ code/src/allreduce_butterfly/impl.cpp
@@ -1,4 +1,6 @@
 #include "allreduce_butterfly/impl.hpp"
+//#include <bitset>
+//#include <iostream>
 
 namespace impls::allreduce_butterfly {
 
@@ -18,6 +20,17 @@ void allreduce_butterfly::compute(const std::vector<vector>& a_in, const std::ve
     fprintf(stderr, "%d: Starting Butterfly numprocs=%d\n", rank, num_procs);
     return;
   }
+
+  int round, recv_rank;
+  int one = 1UL;
+  int matrix_size = N * M;
+  MPI_Status status;
+  // Initial Temporary Matrix
+  auto tempMatrix = matrix::outer(a, b);
+  double* receivedMatrixPtr = new double[N * M];
+  double* tempMatrixPtr = tempMatrix.get_ptr();
+  double* resultPtr = result.get_ptr();
+
   int n_rounds = 0;
   int n_processors = num_procs;
   while (n_processors >>= 1) ++n_rounds;
@@ -27,22 +40,11 @@ void allreduce_butterfly::compute(const std::vector<vector>& a_in, const std::ve
   // - normal butterfly
   // - the extra nodes send their local copy back to the extra nodes
 
-  int round, recv_rank;
-  int one = 1UL;
-  int matrix_size = N * M;
-  MPI_Status status;
-
-  // Initial Temporary Matrix
-  auto tempMatrix = matrix::outer(a, b);
-  double* receivedMatrixPtr = new double[N * M];
-  double* tempMatrixPtr = tempMatrix.get_ptr();
-  double* resultPtr = result.get_ptr();
-
   // Initialization for non-power-of-2 setup
   int power_2_ranks = (1 << n_rounds);
+  int n_idle_ranks = num_procs - power_2_ranks; // number of idle processors
   bool non_power_of_2_rounds = (num_procs != power_2_ranks);
   bool i_am_idle_rank = (rank >= power_2_ranks);  // whether I am idle or not
-  int n_idle_ranks = num_procs - power_2_ranks;   // number of idle processors
   bool i_am_idle_partner = (rank < n_idle_ranks); // whether I am partner of an idle process
   int idle_partner_rank = 0;
   if (i_am_idle_rank) {
@@ -54,15 +56,24 @@ void allreduce_butterfly::compute(const std::vector<vector>& a_in, const std::ve
   // Start Reducing to nearest power of 2
   if (non_power_of_2_rounds && i_am_idle_rank) {
     fprintf(stderr, "%d: [IDLE] Sending to rank=%d\n", rank, idle_partner_rank);
+
+    //    std::cout << "[IDLE] idle_partner_rank = " << std::bitset<32>(idle_partner_rank)  << std::endl;
+
     // send
-    mpi_timer(
-        MPI_Send, tempMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE, comm);
+    //    mpi_timer(MPI_Send, tempMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE,
+    //    comm);
+    MPI_Send(tempMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE, comm);
   }
   if (non_power_of_2_rounds && i_am_idle_partner) {
     fprintf(stderr, "%d: [IDLE-PARTNER] Receiving from rank=%d\n", rank, idle_partner_rank);
+
+    //    std::cout << "[NON-IDLE] idle_partner_rank = " << std::bitset<32>(idle_partner_rank)  << std::endl;
+
     // receive
-    mpi_timer(MPI_Recv, receivedMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE,
-        comm, &status);
+    //    mpi_timer(MPI_Recv, receivedMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank,
+    //    TAG_ALLGATHER_BUTTERFLY_REDUCE,comm, &status);
+    MPI_Recv(
+        receivedMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE, comm, &status);
     // add received data to the  temporary matrix
     for (int i = 0; i < matrix_size; i++) {
       tempMatrixPtr[i] += receivedMatrixPtr[i];
@@ -103,18 +114,16 @@ void allreduce_butterfly::compute(const std::vector<vector>& a_in, const std::ve
 
   // [FINISH Reducing to nearest power of 2]
   if (non_power_of_2_rounds && i_am_idle_partner) {
-
     fprintf(stderr, "%d: [IDLE-PARTNER] Sending to rank=%d\n", rank, idle_partner_rank);
     // send
     mpi_timer(
         MPI_Send, tempMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE, comm);
   }
   if (non_power_of_2_rounds && i_am_idle_rank) {
-
     fprintf(stderr, "%d: [IDLE] REceiving from rank=%d\n", rank, idle_partner_rank);
     // receive --> puts results automatically in tempMatrix
-    mpi_timer(MPI_Recv, tempMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE,
-              comm, &status);
+    mpi_timer(MPI_Recv, tempMatrixPtr, matrix_size, MPI_DOUBLE, idle_partner_rank, TAG_ALLGATHER_BUTTERFLY_REDUCE, comm,
+        &status);
   }
 
   delete[] receivedMatrixPtr;
