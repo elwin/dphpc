13bbc243418984cca4dae3afb613b09e9d0a5adf
Elwin Stephan <elwin.stephan@gmail.com>
2021-11-09 19:08:38 +0100

    go back to old python but install necessary packages grr

--- euler/init.sh
+++ euler/init.sh
@@ -1,4 +1,5 @@
 # call this file using `source init.sh`
 
 source /cluster/apps/local/env2lmod.sh
-module load gcc/9.3.0 cmake/3.20.3 openmpi/4.0.2 python/3.7.4
+module load gcc/9.3.0 cmake/3.20.3 openmpi/4.0.2 python/3.6.5
+pip install --user dataclasses
