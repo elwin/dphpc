f24405bef754270b69080691dfa218c98135a8c9
Elwin Stephan <elwin.stephan@gmail.com>
2021-11-10 11:34:11 +0100

    install dataclasses if not already

--- euler/init.sh
+++ euler/init.sh
@@ -2,4 +2,4 @@
 
 source /cluster/apps/local/env2lmod.sh
 module load gcc/9.3.0 cmake/3.20.3 openmpi/4.0.2 python/3.6.5
-pip install --user dataclasses
+pip list | grep dataclasses || pip install --user dataclasses
