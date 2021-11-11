4d2d78b8e075b14f6a3dfd777cebb1b08891d7bd
davue <davue@bluewin.ch>
2021-11-11 16:56:19 +0100

    Finished but still buggy allreduce-ring

--- code/src/allreduce_ring/impl.cpp
+++ code/src/allreduce_ring/impl.cpp
@@ -1,7 +1,6 @@
 #include "allreduce_ring/impl.hpp"
 
 namespace impls::allreduce {
-
 void allreduce_ring::compute(const std::vector<vector>& a_in, const std::vector<vector>& b_in, matrix& result) {
   const auto& a = a_in[rank];
   const auto& b = b_in[rank];
@@ -27,7 +26,7 @@ void allreduce_ring::compute(const std::vector<vector>& a_in, const std::vector<
     }
 
     // Send current chunk to next node
-    MPI_Send(current + chunk_offset, chunk_length, MPI_DOUBLE, destination, MPI_ANY_TAG, comm);
+    mpi_timer(MPI_Send, current + chunk_offset, chunk_length, MPI_DOUBLE, destination, 0, comm);
 
     // Calculate chunk length of receiving chunk
     chunk_index = chunk_index - 1 % num_procs;
@@ -40,7 +39,7 @@ void allreduce_ring::compute(const std::vector<vector>& a_in, const std::vector<
 
     // Receive chunk from previous node
     auto recv_chunk = new double[chunk_length];
-    MPI_Recv(recv_chunk, chunk_length, MPI_DOUBLE, source, MPI_ANY_TAG, comm, MPI_STATUS_IGNORE);
+    mpi_timer(MPI_Recv, recv_chunk, chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
 
     // Add received chunk to current matrix
     for (int j = 0; j < chunk_length; ++j) {
@@ -48,9 +47,45 @@ void allreduce_ring::compute(const std::vector<vector>& a_in, const std::vector<
     }
   }
 
-  // TODO: Distribute partial results among all nodes
+  // At this point the current node should have the result of the chunk with index (rank + 1).
+  // We then have to distribute all result chunks
   for (int i = 0; i < num_procs; ++i) {
+    auto chunk_index = (rank - i + 1) % num_procs;
 
+    auto chunk_offset = chunk_index * chunk_size;
+    int chunk_length = 0;
+    if (chunk_index == num_procs - 1) {
+      chunk_length = (int)last_chunk_size;
+    } else {
+      chunk_length = (int)chunk_size;
+    }
+
+    // Send current chunk to next node
+    mpi_timer(MPI_Send, current + chunk_offset, chunk_length, MPI_DOUBLE, destination, 0, comm);
+
+    // Calculate chunk length of receiving chunk
+    chunk_index = chunk_index - 1 % num_procs;
+    chunk_offset = chunk_index * chunk_size;
+    if (chunk_index == num_procs - 1) {
+      chunk_length = (int)last_chunk_size;
+    } else {
+      chunk_length = (int)chunk_size;
+    }
+
+    // Receive chunk from previous node
+    auto recv_chunk = new double[chunk_length];
+    mpi_timer(MPI_Recv, recv_chunk, chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
+
+    // Replace received chunk in result
+    for (int j = 0; j < chunk_length; ++j) {
+      current[chunk_offset + j] = recv_chunk[j];
+    }
+  }
+
+  // Copy result into final matrix
+  auto result_ptr = result.get_ptr();
+  for (int i = 0; i < N * M; ++i) {
+    result_ptr[i] = current[i];
   }
 }
 
