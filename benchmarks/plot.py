2db92b778eebe790e8340e3bbb47e13be44b4ace
n0he <noheim@student.ethz.ch>
2021-11-12 14:18:21 +0100

    added scatter possibility to plots and aggregate

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -5,11 +5,112 @@ import typing
 import matplotlib.pyplot as plt
 import pandas as pd
 import pandas.io.json
+import numpy as np
+import math
 
 
 @dataclasses.dataclass
 class PlotManager:
     output_dir: str
+
+    def plot_for_analysis(self, df: pd.DataFrame, func_key = 'median'):
+        self.plot_runtime_with_scatter(df, 'numprocs', 'N', func_key=func_key)
+        self.plot_runtime_with_scatter(df, 'N', 'numprocs', func_key=func_key)
+        self.plot_runtime_by_key(df, 'N', 'numprocs', func_key=func_key)
+        self.plot_runtime_by_key(df, 'numprocs', 'N', func_key=func_key)
+
+   
+    def plot_runtime_by_key(self, df: pd.DataFrame, filter_key = 'N', index_key='numprocs', func_key='mean', powers_of_two = True):
+        if func_key == 'mean':
+            agg_func = np.mean
+        elif func_key == 'median':
+            agg_func = np.median
+        elif func_key == '99':
+            agg_func = self.ninety_nine
+        else:
+            agg_func = np.mean
+        
+        # print(df[filter_key].unique())
+        # print(df)
+        for i in df[filter_key].unique():
+            # print(math.log(i, 2))
+            if powers_of_two and not math.log(i, 2).is_integer():
+                continue
+            
+            # print("entered")
+            data = df[df[filter_key] == i]
+            data.pivot_table(
+                index=index_key,
+                columns='implementation',
+                values='runtime',
+                aggfunc=agg_func
+            ).plot(
+                title=f'Runtime ({i} nodes)',
+                kind='line',
+                logy=True,
+                ylabel='Runtime (s)',
+                xlabel='Input Dimension',
+            )
+            plt.tight_layout()
+            plt.savefig(f'{self.output_dir}/runtime_{i}_{filter_key}_{func_key}.svg')
+            plt.close()
+    
+    def plot_runtime_with_scatter(self, df: pd.DataFrame, filter_key = 'N', index_key='numprocs', func_key='median', powers_of_two = True):
+        if func_key == 'mean':
+            agg_func = np.mean
+        elif func_key == 'median':
+            agg_func = np.median
+        elif func_key == '99':
+            agg_func = self.ninety_nine
+        else:
+            agg_func = np.mean
+        
+        for i in df[filter_key].unique():
+            if powers_of_two and not math.log(i, 2).is_integer():
+                continue
+            data = df[df[filter_key] == i]
+            
+            color_dict = self.map_colors(data['implementation'].unique())
+            fig, ax = plt.subplots()
+            ax.scatter(data[index_key], data['runtime'], color=[color_dict.get(x) for x in data['implementation']])
+            data = df[df[filter_key] == i]
+            data_filtered = data.pivot_table(
+                index=index_key,
+                columns='implementation',
+                values='runtime',
+                aggfunc=agg_func
+            )
+
+            for col in data_filtered.columns:
+                # pprint(data_filtered.index)
+                # pprint(data_filtered[col])
+                x, y = self.removeNanFromCols(data_filtered.index, data_filtered[col])
+                ax.plot(x, y, color=color_dict.get(col), label=col)
+            ax.set_yscale('log')
+            plt.tight_layout()
+            plt.legend(loc="upper left")
+            plt.savefig(f'{self.output_dir}/runtime_{i}_{filter_key}_{func_key}_with_scatter.svg')
+            plt.close()
+
+    def map_colors(self, implementation_types):
+        colors = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'gray', 'orange']
+        color_dict = {}
+        for i in range(len(implementation_types)):
+            color_dict[implementation_types[i]] = colors[i]
+        return color_dict
+
+    def removeNanFromCols(self, index, col):
+        col_arr = col.to_numpy()
+        x = []
+        y = []
+        for i in range(len(index)):
+            if not np.isnan(col_arr[i]):
+                x.append(index[i])
+                y.append(col_arr[i])
+        return x, y
+
+    def ninety_nine(self, p):
+        return np.percentile(p, 99)
 
     def plot_runtime(self, df: pd.DataFrame):
         for i in [2 ** i for i in range(1, 6)]:
@@ -103,5 +204,6 @@ def plot(input_files: typing.List[str], output_dir: str):
     df['fraction'] = df['runtime_compute'] / df['runtime']
 
     pm.plot_runtime(df)
+    pm.plot_for_analysis(df)
     pm.plot_compute_ratio(df)
     pm.plot_mem_usage(df)
