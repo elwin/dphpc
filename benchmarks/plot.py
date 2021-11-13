a69845bdf1d4cfca55acb2442dfed1be68f344d7
Elwin Stephan <elwin.stephan@gmail.com>
2021-11-13 18:54:37 +0100

    cleanup code, plot adjustments

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pandas.io.json
-import numpy as np
 import math
 
 agg_func = np.median
@@ -16,14 +15,14 @@ agg_func = np.median
 class PlotManager:
     output_dir: str
 
-    def plot_for_analysis(self, df: pd.DataFrame, func_key = 'median'):
+    def plot_for_analysis(self, df: pd.DataFrame, func_key='median'):
         self.plot_runtime_with_scatter(df, 'numprocs', 'N', func_key=func_key)
         self.plot_runtime_with_scatter(df, 'N', 'numprocs', func_key=func_key)
         self.plot_runtime_by_key(df, 'N', 'numprocs', func_key=func_key)
         self.plot_runtime_by_key(df, 'numprocs', 'N', func_key=func_key)
 
-   
-    def plot_runtime_by_key(self, df: pd.DataFrame, filter_key = 'N', index_key='numprocs', func_key='mean', powers_of_two = True):
+    def plot_runtime_by_key(self, df: pd.DataFrame, filter_key='N', index_key='numprocs', func_key='mean',
+                            powers_of_two=True):
         if func_key == 'mean':
             agg_func = np.mean
         elif func_key == 'median':
@@ -32,14 +31,14 @@ class PlotManager:
             agg_func = self.ninety_nine
         else:
             agg_func = np.mean
-        
+
         # print(df[filter_key].unique())
         # print(df)
         for i in df[filter_key].unique():
             # print(math.log(i, 2))
             if powers_of_two and not math.log(i, 2).is_integer():
                 continue
-            
+
             # print("entered")
             data = df[df[filter_key] == i]
             data.pivot_table(
@@ -57,8 +56,9 @@ class PlotManager:
             plt.tight_layout()
             plt.savefig(f'{self.output_dir}/runtime_{i}_{filter_key}_{func_key}.svg')
             plt.close()
-    
-    def plot_runtime_with_scatter(self, df: pd.DataFrame, filter_key = 'N', index_key='numprocs', func_key='median', powers_of_two = True):
+
+    def plot_runtime_with_scatter(self, df: pd.DataFrame, filter_key='N', index_key='numprocs', func_key='median',
+                                  powers_of_two=True):
         if func_key == 'mean':
             agg_func = np.mean
         elif func_key == 'median':
@@ -67,12 +67,12 @@ class PlotManager:
             agg_func = self.ninety_nine
         else:
             agg_func = np.mean
-        
+
         for i in df[filter_key].unique():
             if powers_of_two and not math.log(i, 2).is_integer():
                 continue
             data = df[df[filter_key] == i]
-            
+
             color_dict = self.map_colors(data['implementation'].unique())
             fig, ax = plt.subplots()
             ax.scatter(data[index_key], data['runtime'], color=[color_dict.get(x) for x in data['implementation']])
@@ -87,7 +87,7 @@ class PlotManager:
             for col in data_filtered.columns:
                 # pprint(data_filtered.index)
                 # pprint(data_filtered[col])
-                x, y = self.removeNanFromCols(data_filtered.index, data_filtered[col])
+                x, y = self.remove_nan_from_cols(data_filtered.index, data_filtered[col])
                 ax.plot(x, y, color=color_dict.get(col), label=col)
             ax.set_yscale('log')
             plt.tight_layout()
@@ -95,14 +95,16 @@ class PlotManager:
             plt.savefig(f'{self.output_dir}/runtime_{i}_{filter_key}_{func_key}_with_scatter.svg')
             plt.close()
 
-    def map_colors(self, implementation_types):
+    @staticmethod
+    def map_colors(implementation_types):
         colors = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'gray', 'orange']
         color_dict = {}
         for i in range(len(implementation_types)):
             color_dict[implementation_types[i]] = colors[i]
         return color_dict
 
-    def removeNanFromCols(self, index, col):
+    @staticmethod
+    def remove_nan_from_cols(index, col):
         col_arr = col.to_numpy()
         x = []
         y = []
@@ -112,12 +114,15 @@ class PlotManager:
                 y.append(col_arr[i])
         return x, y
 
-    def ninety_nine(self, p):
+    @staticmethod
+    def ninety_nine(p):
         return np.percentile(p, 99)
 
     def plot_runtime(self, df: pd.DataFrame):
         for i in [2 ** i for i in range(1, 6)]:
             data = df[df['numprocs'] == i]
+            if data.shape[0] == 0:
+                continue
 
             data.pivot_table(
                 index='N',
@@ -137,6 +142,8 @@ class PlotManager:
 
         for n in [2 ** n for n in range(4, 14)]:
             data = df[df['N'] == n]
+            if data.shape[0] == 0:
+                continue
 
             data.pivot_table(
                 index='numprocs',
@@ -158,6 +165,8 @@ class PlotManager:
         for i in [2 ** i for i in range(1, 6)]:
             data = df[df['numprocs'] == i]
             data = data[['N', 'implementation', 'job.mem_max_avg', 'job.mem_requested']]
+            if data.shape[0] == 0:
+                continue
 
             data.pivot_table(
                 index='N',
@@ -177,6 +186,8 @@ class PlotManager:
     def plot_compute_ratio(self, df: pd.DataFrame):
         for i in [2 ** i for i in range(1, 6)]:
             data = df[df['numprocs'] == i]
+            if data.shape[0] == 0:
+                continue
 
             data.pivot_table(
                 index='N',
@@ -185,10 +196,29 @@ class PlotManager:
                 aggfunc=agg_func,
             ).plot(
                 title=f'Compute ratio ({i} nodes)',
-                kind='bar',
+                kind='line',
             )
             plt.tight_layout()
             plt.savefig(f'{self.output_dir}/compute_ratio_{i}.svg')
+            plt.close()
+
+        for n in [2 ** n for n in range(4, 14)]:
+            data = df[df['N'] == n]
+            if data.shape[0] == 0:
+                continue
+
+            data.pivot_table(
+                index='numprocs',
+                columns='implementation',
+                values='fraction',
+                aggfunc=agg_func,
+            ).plot(
+                title=f'Compute ratio ({n} vector size)',
+                kind='line',
+                xlabel='Number of processes',
+            )
+            plt.tight_layout()
+            plt.savefig(f'{self.output_dir}/compute_ratio_dim_{n}.svg')
             plt.close()
 
     def plot_repetitions(self, df: pd.DataFrame):
@@ -196,6 +226,8 @@ class PlotManager:
             vector_size = 4096
 
             data = df[(df['M'] == vector_size) & (df['numprocs'] == num_procs)]
+            if data.shape[0] == 0:
+                continue
 
             data.pivot_table(
                 index='iteration',
@@ -206,7 +238,6 @@ class PlotManager:
                 title=f'Runtime across repetition number ({vector_size} vector size, {num_procs} nodes)',
                 ylabel='Runtime (s)',
                 xlabel='Iteration',
-                kind='bar',
             )
             plt.tight_layout()
             plt.savefig(f'{self.output_dir}/runtime_repetition_{num_procs}.svg')
@@ -230,9 +261,12 @@ def plot(input_files: typing.List[str], output_dir: str):
     df['runtime'] /= 1_000_000
     df['fraction'] = df['runtime_compute'] / df['runtime']
     df['iteration'] += 1
+    df = df[df['implementation'] != 'rabenseifner-scatter']
+    df_complete = df.copy()
+    df = df[df['iteration'] > 1]
 
     pm.plot_runtime(df)
-    pm.plot_for_analysis(df)
+    # pm.plot_for_analysis(df)
     pm.plot_compute_ratio(df)
     pm.plot_mem_usage(df)
-    pm.plot_repetitions(df)
+    pm.plot_repetitions(df_complete)
