1ec87c7f272ef3b02440fb2ce7d6710cc0814d63
Patrick Ziegler <p.ziegler96@gmail.com>
2021-11-22 20:08:23 +0100

    Implement bruck algo w/ async MPI (#32)
    * Implement bruck algo w/ async MPI
    
    * Fix CI

--- code/include/common.h
+++ code/include/common.h
@@ -18,12 +18,12 @@ static constexpr int ROOT = 0;
 #define TAG_RABENSEIFNER_GATHER_VEC_A TAG_BASE + 8
 #define TAG_RABENSEIFNER_GATHER_VEC_B TAG_BASE + 9
 #define TAG_RABENSEIFNER_SCATTER TAG_BASE + 10
-#define TAG_ALLGATHER_BUTTERFLY TAG_BASE + 4
+#define TAG_BRUCK_ASYNC TAG_BASE + 11
 
 #ifdef NDEBUG
 #define debug_log(...) ((void)0)
 #else
-#define debug_log(...) fprintf (stderr, __VA_ARGS__)
+#define debug_log(...) fprintf(stderr, __VA_ARGS__)
 #endif
 
 #define HERE(rank, fmt, ...) debug_log("[%d]%s:%d: " fmt "\n", rank, __FILE__, __LINE__, __VA_ARGS__)
