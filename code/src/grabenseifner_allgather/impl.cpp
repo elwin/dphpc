a939e1d8eefa8d4bc3076436d8462b8a5ba2635c
roy-sc <roy-sc@users.noreply.github.com>
2021-11-24 14:54:13 +0100

    Allreduce rabenseifner (#36)
    Made some adaptation to the sum-of-outer-product computation

--- code/src/grabenseifner_allgather/impl.cpp
+++ code/src/grabenseifner_allgather/impl.cpp
@@ -19,18 +19,18 @@ void grabenseifner_allgather::compute(
     fprintf(stderr, "%d: Starting Generalized Rabenseifner numprocs=%d\n", rank, num_procs);
     return;
   }
-  size_t n_processors = num_procs;
-  size_t n_rows = (size_t)a.size();
-  size_t n_cols = (size_t)b.size();
-  size_t my_rank = (size_t)rank;
+  int n_processors = num_procs;
+  int n_rows = (int)a.size();
+  int n_cols = (int)b.size();
+  int my_rank = (int)rank;
   // partition the rows of the final matrix among all processes
   // If there are not enough rows --> divide rows among first n_rows processes
-  size_t my_n_rows = n_rows / n_processors;
-  size_t last_n_rows = n_rows - ((n_processors - 1) * my_n_rows);
-  size_t residual_rows = last_n_rows - my_n_rows;
-  size_t last_chunk_start_row = n_processors * my_n_rows;
-  size_t my_start_row = my_n_rows * rank;
-  size_t my_block_size = my_n_rows * n_cols;
+  int my_n_rows = n_rows / n_processors;
+  int last_n_rows = n_rows - ((n_processors - 1) * my_n_rows);
+  int residual_rows = last_n_rows - my_n_rows;
+  int last_chunk_start_row = n_processors * my_n_rows;
+  int my_start_row = my_n_rows * rank;
+  int my_block_size = my_n_rows * n_cols;
   //  int i_compute = 1; // TODO
   if (n_rows < n_processors) {
     my_n_rows = 1;
@@ -45,7 +45,7 @@ void grabenseifner_allgather::compute(
   }
   // send and receive buffers
   // to avoid two broadcast rounds --> append vectors a, b
-  size_t appended_vec_size = n_rows + n_cols;
+  int appended_vec_size = n_rows + n_cols;
   vector appended_vectors(appended_vec_size);
   double* appended_vecs = appended_vectors.data();
   const double* a_ptr = a.data();
@@ -53,7 +53,7 @@ void grabenseifner_allgather::compute(
   double* result_ptr = result.get_ptr();
   matrix receive_buffer((n_rows + n_cols), n_processors);
   double* receive_buf = receive_buffer.get_ptr();
-  size_t i;
+  int i;
   for (i = 0; i < n_rows; i++) {
     appended_vecs[i] = a_ptr[i];
   }
@@ -66,25 +66,93 @@ void grabenseifner_allgather::compute(
   // allgather round
   mpi_timer(
       MPI_Allgather, appended_vecs, appended_vec_size, MPI_DOUBLE, receive_buf, appended_vec_size, MPI_DOUBLE, comm);
-  size_t row_idx_upper = my_start_row + my_n_rows;
-  size_t row_i = 0;
-  size_t col_i = 0;
-  size_t proc_i = 0;
-  size_t a_base = 0;
-  size_t b_base = 0;
-  for (proc_i = 0; proc_i < n_processors; proc_i++) {
-    a_base = proc_i * appended_vec_size;
-    b_base = proc_i * appended_vec_size + n_rows;
-    for (row_i = my_start_row; row_i < row_idx_upper; row_i++) {
-      for (col_i = 0; col_i < n_cols; col_i++) {
-        result_ptr[row_i * n_cols + col_i] += receive_buf[a_base + row_i] * receive_buf[b_base + col_i];
+  const int GRABENSEIFNER_BSIZE = 16;
+  int row_idx_upper = my_start_row + my_n_rows;
+  int row_idx_upper_blkd = row_idx_upper - GRABENSEIFNER_BSIZE;
+  int n_cols_blkd = n_cols - GRABENSEIFNER_BSIZE;
+  int row_i = 0;
+  int col_i = 0;
+  int row_ii, col_ii;
+  int proc_i = 0;
+  int a_base = 0;
+  int b_base = 0;
+  int row_offset = my_start_row * n_cols;
+  double a_val, b_val, res_val;
+
+  row_i = my_start_row;
+  for (; row_i < row_idx_upper_blkd; row_i += GRABENSEIFNER_BSIZE) {
+    for (col_i = 0; col_i < n_cols_blkd; col_i += GRABENSEIFNER_BSIZE) {
+      for (row_ii = row_i; row_ii < row_i + GRABENSEIFNER_BSIZE; row_ii++) {
+        row_offset = row_ii * n_cols;
+        for (col_ii = col_i; col_ii < col_i + GRABENSEIFNER_BSIZE; col_ii++) {
+          a_base = 0;
+          b_base = n_rows;
+          for (proc_i = 0; proc_i < num_procs; proc_i++) {
+            a_val = receive_buf[a_base + row_ii];
+            b_val = receive_buf[b_base + col_ii];
+            res_val = result_ptr[row_offset + col_ii];
+            res_val += a_val * b_val;
+            result_ptr[row_offset + col_ii] = res_val;
+            a_base += appended_vec_size;
+            b_base += appended_vec_size;
+          }
+        }
+      }
+    }
+    // clean up columns
+    for (; col_i < n_cols; col_i++) {
+      row_offset = row_i * n_cols;
+      for (row_ii = row_i; row_ii < row_i + GRABENSEIFNER_BSIZE; row_ii++) {
+        a_base = 0;
+        b_base = n_rows;
+        for (proc_i = 0; proc_i < num_procs; proc_i++) {
+          a_val = receive_buf[a_base + row_ii];
+          b_val = receive_buf[b_base + col_i];
+          res_val = result_ptr[row_offset + col_i];
+          res_val += a_val * b_val;
+          result_ptr[row_offset + col_i] = res_val;
+          a_base += appended_vec_size;
+          b_base += appended_vec_size;
+        }
+        row_offset += n_cols;
       }
     }
   }
-  //  fprintf(stderr,
-  //      "%d [Special Last Block] my_start_row=%zu, my_n_rows=%zu, n_rows=%zu, n_cols=%zu, last_chunk_start_row=%zu, "
-  //      "residual_rows=%zu, last_n_rows=%zu\n",
-  //      rank, my_start_row, my_n_rows, n_rows, n_cols, last_chunk_start_row, residual_rows, last_n_rows);
+  // clean up rows
+  row_offset = row_i * n_cols;
+  for (; row_i < row_idx_upper; row_i++) {
+    for (col_i = 0; col_i < n_cols; col_i++) {
+      a_base = 0;
+      b_base = n_rows;
+      for (proc_i = 0; proc_i < num_procs; proc_i++) {
+        a_val = receive_buf[a_base + row_i];
+        b_val = receive_buf[b_base + col_i];
+        res_val = result_ptr[row_offset + col_i];
+        res_val += a_val * b_val;
+        result_ptr[row_offset + col_i] = res_val;
+        a_base += appended_vec_size;
+        b_base += appended_vec_size;
+      }
+    }
+    row_offset += n_cols;
+  }
+//
+//    for (row_i = my_start_row; row_i < row_idx_upper; row_i++) {
+//      for (col_i = 0; col_i < n_cols; col_i++) {
+//        a_base = 0;
+//        b_base = n_rows;
+//        for (proc_i = 0; proc_i < num_procs; proc_i++) {
+//          a_val = receive_buf[a_base + row_i];
+//          b_val = receive_buf[b_base + col_i];
+//          res_val = result_ptr[row_offset + col_i];
+//          res_val += a_val * b_val;
+//          result_ptr[row_offset + col_i] = res_val;
+//          a_base += appended_vec_size;
+//          b_base += appended_vec_size;
+//        }
+//      }
+//      row_offset += n_cols;
+//    }
 
   if (special_last_block) {
     // second allgather round matrix prefix
@@ -98,14 +166,6 @@ void grabenseifner_allgather::compute(
     mpi_timer(MPI_Allgather, &result_ptr[my_start_row * n_cols], my_block_size, MPI_DOUBLE, result_ptr, my_block_size,
         MPI_DOUBLE, comm);
   }
-
-  //  auto result_str = result.string();
-  //  auto appended_vec_str = appended_vectors.string();
-  //  auto receive_buffer_str = receive_buffer.string();
-  //  fprintf(stderr, "%d: Current State:\n Sending-Vectors = %s\n Result = %s\n...\n", rank, appended_vec_str.c_str(),
-  //      result_str.c_str());
-
-  //  fprintf(stderr, "%d: Current State:\n Receive buffer = %s\n Sending-Vectors = %s\n Result = %s\n...\n", rank,
-  //      receive_buffer_str.c_str(), appended_vec_str.c_str(), result_str.c_str());
+  
 }
 } // namespace impls::grabenseifner_allgather
