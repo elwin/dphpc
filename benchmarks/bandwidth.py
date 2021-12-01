7174b9ae766bf2dec6cd65beeca933e18e223513
Elwin Stephan <elwin.stephan@gmail.com>
2021-12-01 10:48:56 +0100

    add some preliminiary bandwidth information

---
+++ benchmarks/bandwidth.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def main():
+    df_rcv = pd.read_csv('resources/bandwidth/bw_rcv.csv', sep='\t')
+    df_snd = pd.read_csv('resources/bandwidth/bw_rcv.csv', sep='\t')
+    pass
+
+if __name__ == '__main__':
+    main()
