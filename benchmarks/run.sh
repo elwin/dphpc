bbfa24f8baca57e0da403838924c6f07dd868719
Elwin Stephan <elwin.stephan@gmail.com>
2021-12-04 23:55:25 +0100

    add run script

---
+++ benchmarks/run.sh
@@ -0,0 +1,13 @@
+NODES=${NODES:-8}
+REPETITIONS=25
+ALGORITHMS=(allgather allreduce allreduce-ring g-rabenseifner-allgather g-rabenseifner-subgroup-2 g-rabenseifner-subgroup-4 g-rabenseifner-subgroup-8)
+VECTOR_SIZES=(1000 2000 3000 4000 5000 6000 7000 8000)
+BINARY="./code/build_output/main"
+OUTPUT="results"
+mkdir ${OUTPUT}
+
+for VECTOR_SIZE in "${VECTOR_SIZES[@]}"; do
+  for ALGORITHM in "${ALGORITHMS[@]}"; do
+      mpirun -np "${NODES}" "${BINARY}" -n "${VECTOR_SIZE}" -m "${VECTOR_SIZE}" -t "${REPETITIONS}" -i "${ALGORITHM}" >> "${OUTPUT}/results-${NODES}.json" 2>> "${OUTPUT}/err-${NODES}.log"
+    done
+done
