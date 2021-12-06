cfc792271f35c05aea18604068c9773f64ce8d5d
patrick96 <p.ziegler96@gmail.com>
2021-12-06 18:28:40 +0100

    First pipelined grabenseifner_allgather

--- code/src/grabenseifner_allgather_segmented/impl.cpp
+++ code/src/grabenseifner_allgather_segmented/impl.cpp
@@ -6,8 +6,12 @@
 
 #include "grabenseifner_allgather/impl.hpp"
 
+// Number of bytes in a segment
 static constexpr int SEG_SIZE = 4096;
+// Number of elements in a segment
 static constexpr int SEG_EL = SEG_SIZE / sizeof(double);
+
+static_assert(SEG_SIZE % sizeof(double) == 0);
 
 namespace impls::grabenseifner_allgather_segmented {
 
@@ -19,17 +23,18 @@ void grabenseifner_allgather_segmented::compute(
   const auto& a = a_in[rank];
   const auto& b = b_in[rank];
 
-  int n_processors = num_procs;
   int n_rows = (int)a.size();
   int n_cols = (int)b.size();
 
-  int appended_vec_size = n_rows + n_cols;
+  if (n_rows < num_procs) {
+    throw std::runtime_error("grabenseifner_allgather_segmented: Fewer rows than processes");
+  }
 
   /*
    * If the sent vector size is smaller than the number of elements in a segment, we can just as well use the
    * non-segmented implementation
    */
-  if (appended_vec_size < SEG_EL) {
+  if (b.size() < SEG_EL) {
     grabenseifner_allgather::grabenseifner_allgather impl(comm, rank, num_procs, N, M);
     impl.compute(a_in, b_in, result);
     return;
@@ -37,15 +42,14 @@ void grabenseifner_allgather_segmented::compute(
 
   int my_rank = (int)rank;
   // partition the rows of the final matrix among all processes
-  // If there are not enough rows --> divide rows among first n_rows processes
-  int my_n_rows = n_rows / n_processors;
+  int my_n_rows = n_rows / num_procs;
   // Number of rows the last process is responsible for
-  const int last_n_rows = n_rows - ((n_processors - 1) * my_n_rows);
+  const int last_n_rows = n_rows - ((num_procs - 1) * my_n_rows);
   /*
    * Number of additional rows, the last process has in relation to other processes.
    */
   const int residual_rows = last_n_rows - my_n_rows;
-  const int last_chunk_start_row = n_processors * my_n_rows;
+  const int last_chunk_start_row = num_procs * my_n_rows;
 
   /*
    * Each process is responsible for rows [my_start_row, my_start_row + my_n_rows)
@@ -57,61 +61,82 @@ void grabenseifner_allgather_segmented::compute(
    * allgather round
    */
   const int my_block_size = my_n_rows * n_cols;
-  if (n_rows < n_processors) {
-    my_n_rows = 1;
-    my_start_row = rank;
-  }
 
   bool special_last_block = (my_n_rows != last_n_rows);
-  if (my_rank == n_processors - 1) {
+  if (my_rank == num_procs - 1) {
     my_n_rows = last_n_rows;
   }
-  // send and receive buffers
-  // to avoid two broadcast rounds --> append vectors a, b
-  vector appended_vectors(appended_vec_size);
-  double* appended_vecs = appended_vectors.data();
-  const double* a_ptr = a.data();
-  const double* b_ptr = b.data();
-  double* result_ptr = result.get_ptr();
-  std::vector<double> receive_buf(appended_vec_size * n_processors);
-  int i;
-  for (i = 0; i < n_rows; i++) {
-    appended_vecs[i] = a_ptr[i];
-  }
-  for (i = 0; i < n_cols; i++) {
-    appended_vecs[n_rows + i] = b_ptr[i];
-  }
 
-  // TODO: Handle case when not all processes compute
+  // num_segments = ceil(M / SEG_EL)
+  const int num_segments = (b.size() + SEG_EL - 1) / SEG_EL;
+
+  vector all_as(n_rows * num_procs);
+  /*
+   * Collects all values in the B vectors for all processes.
+   *
+   * The B vector for a process is not stored contiguously, but because of the segmentation, only SEG_EL elements are
+   * contiguous.
+   *
+   * The B vector for process proc_i in segment seg_i starts at index:
+   *
+   * seg_i * SEG_EL * num_procs + proc_i * SEG_EL = SEG_EL * (seg_i * num_procs + proc_i)
+   */
+  vector all_bs(n_cols * num_procs);
+
+  // TODO support general case
+  assert(b.size() % SEG_EL == 0);
 
   // allgather round
-  mpi_timer(MPI_Allgather, appended_vecs, appended_vec_size, MPI_DOUBLE, receive_buf.data(), appended_vec_size,
-      MPI_DOUBLE, comm);
-  const int row_idx_upper = my_start_row + my_n_rows;
-  for (int row_i = my_start_row; row_i < row_idx_upper; row_i++) {
-    for (int col_i = 0; col_i < n_cols; col_i++) {
-      int a_base = 0;
-      int b_base = n_rows;
-      for (int proc_i = 0; proc_i < num_procs; proc_i++) {
-        double a_val = receive_buf[a_base + row_i];
-        double b_val = receive_buf[b_base + col_i];
-        result.get(row_i, col_i) += a_val * b_val;
-        a_base += appended_vec_size;
-        b_base += appended_vec_size;
+
+  // First receive the A vectors
+  mpi_timer(MPI_Allgather, a.data(), a.size(), MPI_DOUBLE, all_as.data(), a.size(), MPI_DOUBLE, comm);
+
+  const int segment_size = SEG_EL * num_procs;
+  // Number of elements (per process) in last segment
+  const int last_segment_elements = SEG_EL - (b.size() % SEG_EL);
+
+  MPI_Request req = MPI_REQUEST_NULL;
+  mpi_timer(MPI_Iallgather, b.data(), SEG_EL, MPI_DOUBLE, all_bs.data(), SEG_EL, MPI_DOUBLE, comm, &req);
+
+  for (int seg_i = 0; seg_i < num_segments; seg_i++) {
+    bool is_last = seg_i == num_segments - 1;
+    // Index in all_bs where this segment starts
+    int base_index = seg_i * segment_size;
+
+    // Wait for previous allgather to complete
+    if (req != MPI_REQUEST_NULL) {
+      MPI_Wait(&req, MPI_STATUS_IGNORE);
+    }
+
+    // Allgather next segment
+    if (!is_last) {
+      mpi_timer(MPI_Iallgather, b.data() + (seg_i + 1) * SEG_EL, SEG_EL, MPI_DOUBLE,
+          all_bs.data() + base_index + segment_size, SEG_EL, MPI_DOUBLE, comm, &req);
+    }
+
+    // Compute from current segment while next allgather is in flight
+    const int row_idx_upper = my_start_row + my_n_rows;
+    for (int proc_i = 0; proc_i < num_procs; proc_i++) {
+      for (int row_i = my_start_row; row_i < row_idx_upper; row_i++) {
+        double a_val = all_as[proc_i * n_rows + row_i];
+        for (int col_i = 0; col_i < SEG_EL; col_i++) {
+          double b_val = all_bs[SEG_EL * (seg_i * num_procs + proc_i) + col_i];
+          result.get(row_i, seg_i * SEG_EL + col_i) += a_val * b_val;
+        }
       }
     }
   }
 
   if (special_last_block) {
     // second allgather round matrix prefix
-    mpi_timer(MPI_Allgather, &result.get(my_start_row, 0), my_block_size, MPI_DOUBLE, result_ptr, my_block_size,
+    mpi_timer(MPI_Allgather, &result.get(my_start_row, 0), my_block_size, MPI_DOUBLE, result.get_ptr(), my_block_size,
         MPI_DOUBLE, comm);
     // last process broadcasts residual chunk to all processes --> last processor is the root of this broadcast
     mpi_timer(
         MPI_Bcast, &result.get(last_chunk_start_row, 0), (residual_rows * n_cols), MPI_DOUBLE, (num_procs - 1), comm);
   } else {
     // second allgather round
-    mpi_timer(MPI_Allgather, &result.get(my_start_row, 0), my_block_size, MPI_DOUBLE, result_ptr, my_block_size,
+    mpi_timer(MPI_Allgather, &result.get(my_start_row, 0), my_block_size, MPI_DOUBLE, result.get_ptr(), my_block_size,
         MPI_DOUBLE, comm);
   }
 }
