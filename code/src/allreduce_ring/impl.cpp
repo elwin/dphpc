efb9b0d2f6a0808e97e3391a2b5ddb40bbf75f37
davue <davue@bluewin.ch>
2021-12-12 22:54:28 +0100

    Cleanup of ring code

--- code/src/allreduce_ring/impl.cpp
+++ code/src/allreduce_ring/impl.cpp
@@ -17,40 +17,30 @@ void allreduce_ring::compute(const std::vector<vector>& a_in, const std::vector<
 
   // Send partial results through the ring until everyone has everything
   for (int i = 0; i < num_procs - 1; ++i) {
-    auto chunk_index = (rank + num_procs - i) % num_procs;
-
     // Determine current chunk offset and length
-    auto chunk_offset = chunk_index * chunk_size;
-    int chunk_length = 0;
-    if (chunk_index == num_procs - 1) {
-      chunk_length = (int)last_chunk_size;
-    } else {
-      chunk_length = (int)chunk_size;
-    }
+    auto snd_chunk_index = (rank + num_procs - i) % num_procs;
+    auto snd_chunk_offset = snd_chunk_index * chunk_size;
+    int snd_chunk_length = snd_chunk_index == num_procs - 1 ? (int)last_chunk_size : (int)chunk_size;
+
+    // Calculate chunk length of receiving chunk
+    auto rcv_chunk_index = (snd_chunk_index + num_procs - 1) % num_procs;
+    auto rcv_chunk_offset = rcv_chunk_index * chunk_size;
+    int rcv_chunk_length = rcv_chunk_index == num_procs - 1 ? (int)last_chunk_size : (int)chunk_size;
 
     // Send current chunk to next node
     MPI_Request sendRequest;
-    mpi_timer(MPI_Isend, current + chunk_offset, chunk_length, MPI_DOUBLE, destination, 0, comm, &sendRequest);
-
-    // Calculate chunk length of receiving chunk
-    chunk_index = (chunk_index + num_procs - 1) % num_procs;
-    chunk_offset = chunk_index * chunk_size;
-    if (chunk_index == num_procs - 1) {
-      chunk_length = (int)last_chunk_size;
-    } else {
-      chunk_length = (int)chunk_size;
-    }
+    mpi_timer(MPI_Isend, current + snd_chunk_offset, snd_chunk_length, MPI_DOUBLE, destination, 0, comm, &sendRequest);
 
     // Receive chunk from previous node
-    auto recv_chunk = new double[chunk_length];
-    mpi_timer(MPI_Recv, recv_chunk, chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
+    auto recv_chunk = new double[rcv_chunk_length];
+    mpi_timer(MPI_Recv, recv_chunk, rcv_chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
 
     // The message should be received so we can wait on it
     mpi_timer(MPI_Wait, &sendRequest, MPI_STATUS_IGNORE);
 
     // Add received chunk to current matrix
-    for (int j = 0; j < chunk_length; ++j) {
-      current[chunk_offset + j] += recv_chunk[j];
+    for (int j = 0; j < rcv_chunk_length; ++j) {
+      current[rcv_chunk_offset + j] += recv_chunk[j];
     }
 
     delete[] recv_chunk;
@@ -59,31 +49,22 @@ void allreduce_ring::compute(const std::vector<vector>& a_in, const std::vector<
   // At this point the current node should have the result of the chunk with index (rank + 1).
   // We then have to distribute all result chunks
   for (int i = 0; i < num_procs - 1; ++i) {
-    auto chunk_index = (rank + num_procs - i + 1) % num_procs;
+    // Determine current chunk offset and length
+    auto snd_chunk_index = (rank + num_procs - i + 1) % num_procs;
+    auto snd_chunk_offset = snd_chunk_index * chunk_size;
+    int snd_chunk_length = snd_chunk_index == num_procs - 1 ? (int)last_chunk_size : (int)chunk_size;
 
-    auto chunk_offset = chunk_index * chunk_size;
-    int chunk_length = 0;
-    if (chunk_index == num_procs - 1) {
-      chunk_length = (int)last_chunk_size;
-    } else {
-      chunk_length = (int)chunk_size;
-    }
+    // Calculate chunk length of receiving chunk
+    auto rcv_chunk_index = (snd_chunk_index + num_procs - 1) % num_procs;
+    auto rcv_chunk_offset = rcv_chunk_index * chunk_size;
+    int rcv_chunk_length = rcv_chunk_index == num_procs - 1 ? (int)last_chunk_size : (int)chunk_size;
 
     // Send current chunk to next node
     MPI_Request sendRequest;
-    mpi_timer(MPI_Isend, current + chunk_offset, chunk_length, MPI_DOUBLE, destination, 0, comm, &sendRequest);
-
-    // Calculate chunk length of receiving chunk
-    chunk_index = (chunk_index + num_procs - 1) % num_procs;
-    chunk_offset = chunk_index * chunk_size;
-    if (chunk_index == num_procs - 1) {
-      chunk_length = (int)last_chunk_size;
-    } else {
-      chunk_length = (int)chunk_size;
-    }
+    mpi_timer(MPI_Isend, current + snd_chunk_offset, snd_chunk_length, MPI_DOUBLE, destination, 0, comm, &sendRequest);
 
     // Receive chunk from previous node
-    mpi_timer(MPI_Recv, current + chunk_offset, chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
+    mpi_timer(MPI_Recv, current + rcv_chunk_offset, rcv_chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
 
     // The message should be received so we can wait on it
     mpi_timer(MPI_Wait, &sendRequest, MPI_STATUS_IGNORE);
