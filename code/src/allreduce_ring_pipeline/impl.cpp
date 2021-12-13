e5e04beda33dba061012abad8e8fbfa822bf9a1a
davue <davue@bluewin.ch>
2021-12-13 10:32:22 +0100

    Finally fixed the pipelined sending for unequal send and receive chunks

--- code/src/allreduce_ring_pipeline/impl.cpp
+++ code/src/allreduce_ring_pipeline/impl.cpp
@@ -35,30 +35,50 @@ void allreduce_ring_pipeline::compute(
     auto rcv_chunk_offset = rcv_chunk_index * chunk_size;
     int rcv_chunk_length = rcv_chunk_index == num_procs - 1 ? (int)last_chunk_size : (int)chunk_size;
 
-    auto pipeline_chunk_count = (snd_chunk_length / SEG_EL) + 1;
-    auto pipeline_remainder = snd_chunk_length % SEG_EL;
+    auto snd_pipeline_chunk_count = (snd_chunk_length / SEG_EL) + 1;
+    auto snd_pipeline_remainder = snd_chunk_length % SEG_EL;
+    if (snd_pipeline_remainder == 0) {
+      snd_pipeline_chunk_count--;
+      snd_pipeline_remainder = SEG_EL;
+    }
+
+    auto rcv_pipeline_chunk_count = (rcv_chunk_length / SEG_EL) + 1;
+    auto rcv_pipeline_remainder = rcv_chunk_length % SEG_EL;
+    if (rcv_pipeline_remainder == 0) {
+      rcv_pipeline_chunk_count--;
+      rcv_pipeline_remainder = SEG_EL;
+    }
+
+    auto pipeline_chunk_count = std::max(snd_pipeline_chunk_count, rcv_pipeline_chunk_count);
     for (int j = 0; j < pipeline_chunk_count; ++j) {
       auto pipeline_chunk_offset = j * SEG_EL;
-      auto pipeline_chunk_length = j == pipeline_chunk_count - 1 ? pipeline_remainder : SEG_EL;
 
-      // Send current chunk to next node
       MPI_Request sendRequest;
-      mpi_timer(MPI_Isend, current + snd_chunk_offset + pipeline_chunk_offset, pipeline_chunk_length, MPI_DOUBLE,
-          destination, 0, comm, &sendRequest);
-
-      // Receive chunk from previous node
-      auto recv_chunk = new double[pipeline_chunk_length];
-      mpi_timer(MPI_Recv, recv_chunk, pipeline_chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
-
-      // The message should be received so we can wait on it
-      mpi_timer(MPI_Wait, &sendRequest, MPI_STATUS_IGNORE);
-
-      // Add received chunk to current matrix
-      for (int k = 0; k < rcv_chunk_length; ++k) {
-        current[rcv_chunk_offset + pipeline_chunk_offset + k] += recv_chunk[k];
+      if (j < snd_pipeline_chunk_count) {
+        // Send current chunk to next node
+        auto snd_pipeline_chunk_length = j == snd_pipeline_chunk_count - 1 ? snd_pipeline_remainder : SEG_EL;
+        mpi_timer(MPI_Isend, current + snd_chunk_offset + pipeline_chunk_offset, snd_pipeline_chunk_length, MPI_DOUBLE,
+            destination, 0, comm, &sendRequest);
       }
 
-      delete[] recv_chunk;
+      if (j < rcv_pipeline_chunk_count) {
+        // Receive chunk from previous node
+        auto rcv_pipeline_chunk_length = j == rcv_pipeline_chunk_count - 1 ? rcv_pipeline_remainder : SEG_EL;
+        auto recv_chunk = new double[rcv_pipeline_chunk_length];
+        mpi_timer(MPI_Recv, recv_chunk, rcv_pipeline_chunk_length, MPI_DOUBLE, source, 0, comm, MPI_STATUS_IGNORE);
+
+        if (sendRequest) {
+          // The message should be received so we can wait on it
+          mpi_timer(MPI_Wait, &sendRequest, MPI_STATUS_IGNORE);
+        }
+
+        // Add received chunk to current matrix
+        for (int k = 0; k < rcv_chunk_length; ++k) {
+          current[rcv_chunk_offset + pipeline_chunk_offset + k] += recv_chunk[k];
+        }
+
+        delete[] recv_chunk;
+      }
     }
   }
 
